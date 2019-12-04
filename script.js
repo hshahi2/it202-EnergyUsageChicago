9955e3429a0dbf60f414b8da6c6227b5f048dda8
Hamza Shahid <hshahi2@uic.edu>
2019-12-04 06:02:08 +0000

    added names in listview

---
+++ script.js
@@ -0,0 +1,10 @@
+let ctaTrainLines = {
+  "red": {"code":"Red", "title":"Red Line (Howard-95th/Dan Ryan service)"},
+  "blue": {"code":"Blue","title":"Blue Line (O’Hare-Forest Park service)"},
+  "brown": {"code":"Brn", "title": "Brown Line (Kimball-Loop service)"},
+  "green": {"code":"G", "title": "Green Line (Harlem/Lake-Ashland/63rd-Cottage Grove service)"},
+  "orange": {"code":"Org", "title": "Orange Line (Midway-Loop service)"},
+  "purple":{"code":"P","title":"Purple Line (Linden-Howard shuttle service)"},
+  "pink": {"code":"Pink", "title":"Pink Line (54th/Cermak-Loop service)"},
+  "yellow": {"code":"Y", "title": "Yellow Line (Skokie-Howard [Skokie Swift] shuttle service)"}
+}
